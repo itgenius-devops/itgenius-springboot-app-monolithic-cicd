2a003f4f4acebd363c55281e9cdece2af4b9e05f
itgenius-devops <info@itgenius.io>
2024-12-06 04:15:17 +0000

    Update variables.tf

--- terraform/terraform-config-files/variables.tf
+++ terraform/terraform-config-files/variables.tf
@@ -5,13 +5,13 @@ variable "region" {
 
 variable "vpc_id" {
   description = "The ID of the VPC where resources will be created"
-  default     = "vpc-060d68cf8f4f0ff4b"
+  default     = "vpc-042e20b92a177e5bb"
 }
 
 variable "subnet_ids" {
   description = "The list of subnet IDs to associate with resources"
   type        = list(string)
-  default     = ["subnet-0305d0946c139cda7", "subnet-0413b34ddd5fbc136", "subnet-085dd956e5b639f40", "subnet-029b9641533fc3b1e"]
+  default     = ["subnet-038402fb5b115020c", "subnet-07a3d5bfa59b05d53", "subnet-0f8468f34ec3b6d6f", "subnet-0616d37dba1488ff8"]
 }
 
 variable "instance_ami" {
@@ -26,7 +26,7 @@ variable "instance_key_name" {
 
 variable "instance_subnet_id" {
   description = "Public Subnet ID for the instances"
-  default     = "subnet-0305d0946c139cda7"
+  default     = "subnet-038402fb5b115020c"
 }
 
 variable "instance_type" {
