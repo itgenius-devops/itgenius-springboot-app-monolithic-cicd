46d205e274758fa8b49f177acc67669130c5b9b7
ITGenius <info@itgenius.io>
2025-07-09 23:03:23 +0100

    Update backend.tf

--- terraform/terraform-config-files/backend.tf
+++ terraform/terraform-config-files/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "itgenius-app-statefile-s3-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
-    dynamodb_table = "itgenius-app-statefile-dynamoDB"
+    use_lockfile   = true
   }
 }
 
